bee81234f7a277d9c6a3cb508325b1aa4b89c93b
bengao10 <86125057+bengao10@users.noreply.github.com>
2022-03-04 11:39:33 -0800

    Distribution elements now visible

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 #############################
 ## Written by Benjamen Gao
 ## For Math 101 Winter 2022
-## Last Edit: B^2 2/9/2022
-## Notes: Second Iteration
+## Last Edit: B^2 3/4/2022
+## Notes: Now Third Iteration
 #############################
-import math
 
+import math
 import cvxpy as cp
 import matplotlib.pyplot as plt
 
-ORDER = ["top left", "top right", "bottom right", "bottom left"]
-
 
 def main():
-    # find_distribution()
-    manual_input()
+    find_distribution()
+    # manual_input()
 
 
 def find_distribution():
-    sums = 100
+    sums = 50
     totals = dict()
+    record = dict()
     for i in range(sums):
         print(f"Currently at {i}")
         for j in range(sums):
             for x in range(sums):
                 for y in range(sums):
-                    callable([i, j, x, y], totals)
+                    callable([i, j, x, y], totals, record)
     plot(totals)
     print(totals)
+    while True:
+        file = open('totals.txt', 'w')
+        val = int(input("What number would you like to see? "))
+        all_vals = record[val]
+        for elem in all_vals:
+            file.write(str(elem))
+            file.write('\n')
 
 
 def plot(totals):
@@ -62,13 +68,14 @@ def return_max(config):
 def distance_test(config):
     x = cp.Variable(1)
     objective = cp.Minimize((config[0] - x) ** 2 + (config[1] - x) ** 2 + (config[2] - x) ** 2 + (config[3] - x) ** 2)
-    constraints = [-10000 <= x, x <= 10000]
+    constraints = [-100 <= x, x <= 100]
     prob = cp.Problem(objective, constraints)
     prob.solve()
     return prob.value
 
 
-def callable(nums, step_tracker):
+def callable(nums, step_tracker, record):
+    starting = nums
     total_steps = 0
     while True:
         if nums[0] == 0 and nums[1] == 0 and nums[2] == 0 and nums[3] == 0:
@@ -77,8 +84,10 @@ def callable(nums, step_tracker):
         nums = [abs(nums[0] - nums[1]), abs(nums[1] - nums[2]), abs(nums[2] - nums[3]), abs(nums[3] - nums[0])]
     if total_steps in step_tracker:
         step_tracker[total_steps] += 1
+        record[total_steps].append(starting)
     else:
-        step_tracker[total_steps] = 0
+        step_tracker[total_steps] = 1
+        record[total_steps] = [starting]
 
 
 if __name__ == '__main__':
--- n-gon.py
+++ n-gon.py
@@ -1,33 +1,42 @@
 import random
 
-n = 128
 max_iter = 10000
 
-
 def main():
+    all_converged = []
+    for i in range(1, 1000):
+        print(i)
+        if converged(i):
+            all_converged.append(i)
+    print(all_converged)
+
+
+def converged(n):
     arr = [random.randint(-100, 100) for _ in range(n)]
     total_iter = 0
     while not check_convergence(arr):
-        arr = do_iteration(arr)
+        arr = do_iteration(arr, n)
         total_iter += 1
         if total_iter == max_iter:
-            break
-    print(f"Configuration: {arr}")
+            return False
+            # break
+    #print(f"Configuration: {arr}")
     if check_convergence(arr):
-        print(f"Converged in {total_iter} iterations")
+        #print(f"Converged in {total_iter} iterations")
+        return True
 
 
 def check_convergence(arr):
-    maximum = max(arr)
+    #maximum = max(arr)
     for elem in arr:
         if elem != 0:
-            print(f"Not converged. Distance: {maximum}")
+            #print(f"Not converged. Distance: {maximum}")
             return False
-    print(f"Converged. Distance: {maximum}")
+    #print(f"Converged. Distance: {maximum}")
     return True
 
 
-def do_iteration(arr):
+def do_iteration(arr, n):
     new_arr = [0 for _ in range(n)]
     for i in range(n):
         if i != n - 1:
